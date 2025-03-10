acdae6b2cf5740aee19545cbb9d20f5e44dfdd88
truongletuankiet <truongletuankiet.la@gmail.com>
2025-03-11 02:49:12 +0700

    Fix for new BE

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,12 @@
-import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["pm-s3-images.s3.us-east-2.amazonaws.com", "scontent.fsgn1-1.fna.fbcdn.net"] // Thêm domain Facebook
+    remotePatterns: [
+      {
+        hostname: "**", // Cho phép tất cả các domain
+      },
+    ],
   },
 };
 
 module.exports = nextConfig;
-
-
-export default nextConfig;
